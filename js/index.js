621af19ca6f283f08e641d1e1dc92336d4e33e3a
Ana Ferrandiz Pascual <anaferrandizpascual@MacBook-Pro-de-Ana.local>
2025-03-05 21:34:36 +0100

    Starting tabs

--- js/index.js
+++ js/index.js
@@ -123,3 +123,16 @@ document.querySelectorAll('.tab').forEach(tab => {
         }
     });
 });
+
+
+// --------- PESTAÑAS
+
+
+function toggleTab(tabId) {
+    var content = document.getElementById(tabId);
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
